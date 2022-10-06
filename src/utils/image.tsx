d3515e046e00125f8670d3f5141424a17ed21a01
denispapakul <denis.papakul.dp@gmail.com>
2022-10-06 09:46:04 +0200

    Fix image displaying

--- src/utils/image.tsx
+++ src/utils/image.tsx
@@ -1,6 +1,6 @@
 export const getImage = (lock?: string, size: number = 400) => {
-  let image = `https://loremflickr.com/${size}/${size}/food${
-    lock ? '?lock=${lock}' : ''
+  const image = `https://loremflickr.com/${size}/${size}/food${
+    lock ? `?lock=${lock}` : ''
   }`;
 
   return image;
