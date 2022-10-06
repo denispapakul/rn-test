abc13f48389cc5cb5d54e292502e81ccb5697db5
denispapakul <denis.papakul.dp@gmail.com>
2022-10-06 11:50:12 +0200

    Optimize sizes of images

--- src/utils/image.tsx
+++ src/utils/image.tsx
@@ -1,5 +1,6 @@
-export const getImage = (lock?: string, size: number = 400) => {
-  const image = `https://loremflickr.com/${size}/${size}/food${
+export const getImage = (lock?: string, size: number = 60) => {
+  const intSize = size.toFixed(0);
+  const image = `https://loremflickr.com/${intSize}/${intSize}/food${
     lock ? `?lock=${lock}` : ''
   }`;
 
